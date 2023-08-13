5a1b9a8457561f6e3339657009be4c2973dc5e67
Iomar Igor Alegre Barrera <42124277+iomarigor@users.noreply.github.com>
2023-08-12 23:43:35 -0500

    auth implement

--- src/libs/history.js
+++
@@ -1,6 +0,0 @@
-/**
- * return history navigation in app
- * @module Herlpers Global
- */
-import * as history from "history";
-export default history.createBrowserHistory();
--- src/pages/Error-404/Error404.Page.jsx
+++ src/pages/Error-404/Error404.Page.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import history from "../../libs/history";
 function Error404Page(props) {
   return <h1>Error 404</h1>;
 }
