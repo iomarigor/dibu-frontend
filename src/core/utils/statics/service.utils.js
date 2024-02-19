efb379d07e0a554dca6b8d3c102d6c91a4aa9cc3
Diego Valencia <diego.valencia@unas.edu.pe>
2024-02-19 15:29:03 -0500

    Creacion de la vista del modulo servicio

---
+++ src/core/utils/statics/service.utils.js
@@ -0,0 +1,57 @@
+import { Becas, Comedor, Complejo, OBU01, Odontologia, Psicopedagogia, Residencia, Social, Topico } from "../../../assets/img/exportIMG";
+
+export const ListaServicios=[
+    {
+      titulo:"Direccion de Bienestar Universitario",
+      imagen:OBU01,
+      url: "https://portalweb.unas.edu.pe/content/dbu"
+    },
+    {
+      titulo:"Servicio Social",
+      imagen:Social,
+      url: "https://portalweb.unas.edu.pe/content/dbu/servicio-social",
+    },
+
+    {
+      titulo:"Topico",
+      imagen:Topico,
+      url:"https://portalweb.unas.edu.pe/content/dbu/topico",
+    },
+
+    {
+      titulo:"Area de Psicopedagogia",
+      imagen:Psicopedagogia,
+      url:"https://portalweb.unas.edu.pe/content/dbu/psicopedagogia",
+    },
+
+    {
+      titulo:"Odontologia",
+      imagen:Odontologia,
+      url:"https://portalweb.unas.edu.pe/content/dbu/odontologia",
+    },
+
+    {
+      titulo:"Comedor Universitario",
+      imagen:Comedor,
+      url:"https://portalweb.unas.edu.pe/content/dbu/comedor-universitario",
+    },
+
+    {
+      titulo:"Residencia Estudiantil",
+      imagen:Residencia,
+      url:"https://portalweb.unas.edu.pe/content/dbu/residencia-estudiantil",
+    },
+
+    {
+      titulo:"Complejo Deportivo",
+      imagen:Complejo,
+      url:"https://portalweb.unas.edu.pe/content/dbu/complejo-deportivo",
+    },
+
+    {
+      titulo:"Becas para Estudio de Pregrado",
+      imagen:Becas,
+      url:"https://portalweb.unas.edu.pe/content/becas-pregrado",
+    },
+
+  ]
