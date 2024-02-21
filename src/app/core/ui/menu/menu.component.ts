331eb86bb01ffa58a390a6b3e91d9d52191fdeba
Iomar Igor Alegre Barrera <42124277+iomarigor@users.noreply.github.com>
2024-02-21 17:04:14 -0500

    implement router link an router active en los items del menu de home

--- src/app/core/ui/menu/menu.component.ts
+++ src/app/core/ui/menu/menu.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
-
 }
